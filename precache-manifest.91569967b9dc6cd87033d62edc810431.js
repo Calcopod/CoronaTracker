c17ed304e2b3118d6d78c3a9d0a631d37d4a4f8c
CalcoBoii <rosca.serban2@gmail.com>
2020-07-22 17:41:16 +0300

    Updates

---
+++ precache-manifest.91569967b9dc6cd87033d62edc810431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f5eaf701aa77dc3b349731dbe133e9",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "75293c0bbbf89f535bb9",
+    "url": "/CoronaTracker/static/css/main.ccfe55da.chunk.css"
+  },
+  {
+    "revision": "999ff0d00ae8d8943b29",
+    "url": "/CoronaTracker/static/js/2.79704197.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaTracker/static/js/2.79704197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75293c0bbbf89f535bb9",
+    "url": "/CoronaTracker/static/js/main.e1370574.chunk.js"
+  },
+  {
+    "revision": "78efa23f5fafbcaa3131",
+    "url": "/CoronaTracker/static/js/runtime-main.3c126882.js"
+  },
+  {
+    "revision": "1b4634b8b30206e5b341bddb6f97e666",
+    "url": "/CoronaTracker/static/media/corona.1b4634b8.png"
+  }
+]);
